1d524f594f960276dfe0d49ce4eab1cf994ef01f
ryangle <hcg1228@foxmail.com>
2020-04-29 14:42:04 +0800

    add code

---
+++ src/Shopshub.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shopshub.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
