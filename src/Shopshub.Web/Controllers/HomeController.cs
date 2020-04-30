a5d185e98217c2ef8d0771128258630a554f4ba4
ryangle <hcg1228@foxmail.com>
2020-04-30 18:33:19 +0800

    configuration site name

--- src/Shopshub.Web/Controllers/HomeController.cs
+++ src/Shopshub.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Shopshub.Web.Models;
 
@@ -12,15 +13,18 @@ namespace Shopshub.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            ViewData["Title"] = "Shops Hub";
+            ViewData["Title"] = "Home";
+            ViewData["SiteName"] = _configuration["SiteName"];
             return View();
         }
 
