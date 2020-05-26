e6059e2acfbbfd3d54e26ebfea4cc38e833cae3e
ryangle <hcg1228@foxmail.com>
2020-05-26 21:06:02 +0800

    add echarts

---
+++ src/Shopshub.Web/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Shopshub.Application;
+using Shopshub.Web.Models;
+
+namespace Shopshub.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
